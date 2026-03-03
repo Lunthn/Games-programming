b3b37040516684ba8d7043ac1cb1450d83584c8b
Nathan Lupke <nr.lupke@gmail.com>
2026-03-03 19:58:20 +0100

    Some cleanup and use 1 axis class

---
+++ Computer Graphics/C#/MatrixTransformations/Axis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixTransformations
+{
+    public class Axis
+    {
+        private string label;
+        private Color color;
+        public List<Vector> vertexbuffer;
+
+        public Axis(int x, int y, int z, string label, Color color)
+        {
+            this.label = label;
+            this.color = color;
+
+            vertexbuffer = new List<Vector>();
+            vertexbuffer.Add(new Vector(0, 0, 0));
+            vertexbuffer.Add(new Vector(x, y, z));
+        }
+
+        public void Draw(Graphics g, List<Vector> vb)
+        {
+            using (Pen pen = new Pen(color, 2f))
+            using (Font font = new Font("Arial", 10))
+            using (SolidBrush brush = new SolidBrush(color))
+            {
+                g.DrawLine(pen, vb[0].x, vb[0].y, vb[1].x, vb[1].y);
+                PointF p = new PointF(vb[1].x, vb[1].y);
+                g.DrawString(label, font, brush, p);
+            }
+        }
+    }
+}
